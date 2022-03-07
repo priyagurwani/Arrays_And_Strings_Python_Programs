d430bfa3b0b0bff6d8d167e285f1094640dd90b1
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-03-07 23:13:23 +0530

    Update 2_second_smallest_element_in_array.py

--- 2_second_smallest_element_in_array.py
+++ 2_second_smallest_element_in_array.py
@@ -1,4 +1,12 @@
+#First Way:
 a=[22,12,2,33,44,1]
 a.sort()
 print(a)
 print(a[1])
+
+#Second Way:
+arr=[11,12,33,55,1,3]
+# print(min(arr))
+arr.remove(min(arr))
+print("The second smallest element of an array is",min(arr))
+
