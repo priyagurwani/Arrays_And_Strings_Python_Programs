89a1c4a57765840c7c0bfd52e42699e91d26c496
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-03-23 23:30:59 +0530

    ord and chr Done:)

---
+++ 24_ASCII_value.py
@@ -0,0 +1,12 @@
+# Using ord get the ASCII value of alphabets
+# ord : convert character in integer
+ch=input("Enter Character: ")
+# ch='A'
+print("The ASCII value of",ch ,"is",ord(ch))
+
+
+# Using chr get the alphabets of decimal number
+# chr : convert integer in character
+num=int(input("ENter num: "))
+# num=65
+print("The character at ASCII value",num,"is",chr(num))
