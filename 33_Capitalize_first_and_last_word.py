d7ee9549aa1d752d0d41c539cb0d9a4418461e0b
priyagurwani <84342895+priyagurwani@users.noreply.github.com>
2022-04-01 23:49:55 +0530

    Capitalize first and last Letter of a String:)

---
+++ 33_Capitalize_first_and_last_word.py
@@ -0,0 +1,7 @@
+# Using capitalize method
+s="heyhappiness"
+print(s[0].capitalize()+s[1:len(s)-1]+ s[-1].capitalize())
+
+# Using upper keyword
+String='heyyou'
+print(String[0:1].upper() + String[1:len(String)-1]+String[len(String)-1:len(String)].upper())
